585b950b93d817ab3be38f4553c81405878557a0
OceanH <1404939185@qq.com>
2024-08-20 23:44:09 +0800

    feat: update 实现bind

--- 手写题/实现bind/index.js
+++ 手写题/实现bind/index.js
@@ -1,5 +1,5 @@
 /**
- * @Description: call和apply的实现与bind类似，在这里不做单独实现
+ * @Description: 实现一个bind方法，需要处理 当bind返回的函数被用作构造函数时的情况
  * @Author: OceanH
  * @Date: 2024-08-20 22:17:58
  */
@@ -9,29 +9,14 @@ Function.prototype.myBind = function (thisArg, ...args) {
   thisArg[key] = fn;
 
   return function newFn(...newFnArgs) {
-    if (this instanceof newFn) {
-      // 加一个判断，用于处理bind返回的函数被用作构造函数的情况
+    // if (this instanceof newFn) {
+    //   // 处理当bind返回的函数被用作构造函数时的情况
+    //   return new fn(...args, ...newFnArgs);
+    // }
+    if (new.target === newFn) {
+      // 处理当bind返回的函数被用作构造函数时的情况
       return new fn(...args, ...newFnArgs);
     }
     return thisArg[key](...args, ...newFnArgs);
   };
 };
-
-//在fnc的原型上定义一个方法
-function func(...arg) {
-  console.log(this)
-  console.log(arg)
-}
-func.prototype.test = function() {
-  console.log(this)
-}
-
-//下面我们使用构造函数测试一下
-let newFunc1 = func.bind({a:1},1,2,3,4) // 原生的bind
-let newFunc2 = func.myBind({a:1},1,2,3,4) // 我们的bind
-let f1 = new newFunc1(5,6,7,8)
-let f2 = new newFunc2(5,6,7,8)
-console.log('-----原生bind-----')
-console.log(f1.test) // 正确的test方法
-console.log('-----myBind-----')
-console.log(f2.test) // undefined
